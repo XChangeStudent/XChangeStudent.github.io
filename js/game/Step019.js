b58500f895a71f4af8f181beb69c467d165351d6
Jebik <jebik@hotmail.fr>
2018-01-25 03:06:32 +0100

    Engenier

---
+++ js/game/Step019.js
@@ -0,0 +1,67 @@
+var path = 'img/Step' + padZero(window.Game.step, window.Padding)+'/';
+
+play = function(g)
+{
+  HideAll();
+  if (g.Job != 'Fitness')
+  {
+    MustChangeStep(1);
+    return;
+  }
+  DenialSolo
+  switch (g.subStep)
+  {
+    case 1:
+      window.CancelBtn.html('Subject');
+      window.CancelBtn.show();
+      window.ValidBtn.html('Engineer');
+      window.ValidBtn.show();
+      window.ImgBox.attr('src', path + g.subStep + '.jpg');
+      window.ImgBox.show();
+      break;
+    case 2:
+      window.ValidBtn.html('Next');
+      window.ValidBtn.show();
+      window.ImgBox.attr('src', path + g.subStep + '.gif');
+      window.ImgBox.show();
+      break;
+    case 3: case 4: case 5:
+      if (g.subStep == 3)
+      {
+        g.DenialSolo++;
+      }
+      window.ValidBtn.html('Next');
+      window.ValidBtn.show();
+      window.VideoBox.children("source").attr('src', path + g.subStep + '.mp4');
+      window.VideoBox.show();
+      window.VideoBox.trigger('load');
+      g.subStep = 9;
+      break;
+    case 6: case 9:
+      window.ValidBtn.html('Next');
+      window.ValidBtn.show();
+      window.ImgBox.attr('src', path + g.subStep + '.jpg');
+      window.ImgBox.show();
+      break;
+    case 7: case 8:
+      window.ValidBtn.html('Next');
+      window.ValidBtn.show();
+      window.VideoBox.children("source").attr('src', path + g.subStep + '.mp4');
+      window.VideoBox.show();
+      window.VideoBox.trigger('load');
+      break;
+    default:
+      alert('Error! Please create an issue on github with these info: Step=' + g.Step + ' subStep=' + g.subStep);
+      break;
+    }
+};
+
+CancelBtnClick = function () {
+  window.Game.subStep = 6;
+  play(window.Game);
+}
+
+ValidBtnClick = function () {
+  if (!MustChangeStep(9))
+    play(window.Game);
+}
