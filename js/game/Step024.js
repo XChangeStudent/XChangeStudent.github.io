524f488bf3d96309bcd3097e36f87797dee44a7d
Jebik <jebik@hotmail.fr>
2018-01-25 04:30:10 +0100

    forgety one

--- js/game/Step024.js
+++ js/game/Step024.js
@@ -41,7 +41,6 @@ play = function(g)
       window.VideoBox.children("source").attr('src', path + g.subStep + '.mp4');
       window.VideoBox.show();
       window.VideoBox.trigger('load');
-      g.subStep = 6;
       break;
     default:
       alert('Error! Please create an issue on github with these info: Step=' + g.Step + ' subStep=' + g.subStep);
